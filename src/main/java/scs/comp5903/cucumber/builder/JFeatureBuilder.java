e8e429007966d7464987757fbaa1a90aa65e4ce6
CXwudi <cx2010017@gmail.com>
2022-10-26 19:10:22 -0400

    [code] even better error message

--- src/main/java/scs/comp5903/cucumber/builder/JFeatureBuilder.java
+++ src/main/java/scs/comp5903/cucumber/builder/JFeatureBuilder.java
@@ -87,7 +87,8 @@ public class JFeatureBuilder {
       var step = stepsCopy.poll();
       var matchResult = tryMatchStep(step, jStepDefMethodDetails).orElseThrow(
           () -> new EasyCucumberException(ErrorCode.EZCU013, "Step definition not found for: " + step
-              + ". Are you sure you implemented this step definition? Or did you forget to make the method public?")
+              + ". Are you sure you implemented this step definition? Or did you forget to make the method public? "
+              + "Or did you forget to add the annotation to method?")
       );
       results.put(step, matchResult);
     }
