e8fe5c62724ad473936edca80b8488042b93a9c5
CXwudi <cx2010017@gmail.com>
2022-10-28 00:01:01 -0400

    add HookType

---
+++ src/main/java/scs/comp5903/cucumber/model/jstepdef/HookType.java
@@ -0,0 +1,15 @@
+package scs.comp5903.cucumber.model.jstepdef;
+
+
+/**
+ * @author Charles Chen 101035684
+ * @date 2022-10-27
+ */
+public enum HookType {
+  BEFORE_ALL_JSCENARIOS,
+  AFTER_ALL_JSCENARIOS,
+  BEFORE_EACH_JSCENARIO,
+  AFTER_EACH_JSCENARIO,
+  BEFORE_EACH_JSTEP,
+  AFTER_EACH_JSTEP;
+}
