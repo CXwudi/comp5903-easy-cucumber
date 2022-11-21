747741a44e73bcea5fd11225a476ad8ab667c20a
CXwudi <cx2010017@gmail.com>
2022-11-20 20:45:12 -0500

    [code+test] enhanced JHookMethodExecution

--- src/main/java/scs/comp5903/cucumber/execution/JHookMethodExecution.java
+++ src/main/java/scs/comp5903/cucumber/execution/JHookMethodExecution.java
@@ -1,5 +1,6 @@
 package scs.comp5903.cucumber.execution;
 
+import java.lang.invoke.MethodType;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Objects;
@@ -18,8 +19,21 @@ public class JHookMethodExecution {
     this.instance = instance;
   }
 
-  public void executeWithParameters(Object... args) throws InvocationTargetException, IllegalAccessException {
-    method.invoke(instance, args);
+  /**
+   * @param args optionally apply the parameter to the hook method
+   */
+  public void executeOnParametersMatch(Object... args) throws InvocationTargetException, IllegalAccessException {
+    var expectedTypes = method.getParameterTypes();
+    if (expectedTypes.length == args.length) {
+      var isAssignable = true;
+      // check each parameter type with consideration of boxed types
+      for (int i = 0; i < expectedTypes.length; i++) {
+        isAssignable = isAssignable && MethodType.methodType(expectedTypes[i]).wrap().returnType().equals(args[i].getClass());
+      }
+      if (isAssignable) {
+        method.invoke(instance, args);
+      }
+    }
   }
 
   @Override
---
+++ src/test/java/scs/comp5903/cucumber/execution/JHookMethodExecutionTest.java
@@ -0,0 +1,26 @@
+package scs.comp5903.cucumber.execution;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Charles Chen 101035684
+ * @date 2022-11-20
+ */
+class JHookMethodExecutionTest {
+
+  public void testMethod(String a, int b) {
+    throw new RuntimeException("test");
+  }
+
+  @Test
+  void canExecuteOnMatching() throws NoSuchMethodException {
+    JHookMethodExecution execution = new JHookMethodExecution(this.getClass().getMethod("testMethod", String.class, int.class), this);
+    var exp = assertThrows(InvocationTargetException.class, () -> execution.executeOnParametersMatch("a", 1));
+    assertEquals(RuntimeException.class, exp.getCause().getClass());
+  }
+}
