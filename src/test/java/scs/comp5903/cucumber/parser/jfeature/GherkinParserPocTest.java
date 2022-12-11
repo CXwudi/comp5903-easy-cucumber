8cd0a985ffe79602ae3f97a0f6c50221589b0b20
CXwudi <cx2010017@gmail.com>
2022-12-11 18:06:53 -0500

    pushed POC first

--- src/test/java/scs/comp5903/cucumber/parser/jfeature/GherkinParserPocTest.java
+++ src/test/java/scs/comp5903/cucumber/parser/jfeature/GherkinParserPocTest.java
@@ -3,6 +3,7 @@ package scs.comp5903.cucumber.parser.jfeature;
 import io.cucumber.gherkin.GherkinParser;
 import io.cucumber.messages.types.Envelope;
 import io.cucumber.messages.types.Source;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import scs.comp5903.cucumber.util.ResourceUtil;
@@ -20,7 +21,7 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @author Charles Chen 101035684
  * @date 2022-12-09
  */
-
+@Disabled("This is a POC test, not a real test")
 class GherkinParserPocTest {
 
   private static final Logger log = getLogger(GherkinParserPocTest.class);
@@ -34,11 +35,52 @@ class GherkinParserPocTest {
     log.info("feature = {}", envelopeList.get(1).getGherkinDocument().orElseThrow().getFeature().orElseThrow());
   }
 
+  @Test
+  void regressionFromSwitchingFromOldImpl() throws URISyntaxException, IOException {
+    var gherkinParser = GherkinParser.builder().build();
+    var path = ResourceUtil.getResourcePath("sample/jfeature/multi-line-description-with-tags-old.jfeature");
+    var envelopeList = gherkinParser.parse(path).collect(Collectors.toList());
+    envelopeList.forEach(envelope -> log.info("{}", envelope));
+  }
+
+  @Test
+  void parseOutlineTable() throws URISyntaxException, IOException {
+    var gherkinParser = GherkinParser.builder().build();
+    var path = ResourceUtil.getResourcePath("sample/jfeature/sample-tagged-scenarios.jfeature");
+    var gherkinDocument = gherkinParser.parse(path).filter(envelope -> envelope.getGherkinDocument().isPresent())
+        .findFirst().orElseThrow().getGherkinDocument().orElseThrow();
+    log.info("gherkinDocument = {}", gherkinDocument);
+  }
+
+  @Test
+  void onEmptyFile() throws URISyntaxException, IOException {
+    var gherkinParser = GherkinParser.builder().build();
+    var path = ResourceUtil.getResourcePath("sample/jfeature/empty-file.jfeature");
+    var envelopeList = gherkinParser.parse(path).collect(Collectors.toList());
+    log.info("envelopeList = {}", envelopeList);
+  }
+
+  @Test
+  void onEmptyFile2() throws URISyntaxException, IOException {
+    var gherkinParser = GherkinParser.builder().build();
+    var path = ResourceUtil.getResourcePath("sample/jfeature/zero-scenarios.jfeature");
+    var envelopeList = gherkinParser.parse(path).collect(Collectors.toList());
+    log.info("envelopeList = {}", envelopeList);
+  }
+
+  @Test
+  void parseWithError() throws URISyntaxException, IOException {
+    var gherkinParser = GherkinParser.builder().build();
+    var path = ResourceUtil.getResourcePath("sample/jfeature/with-error.jfeature");
+    var envelopeList = gherkinParser.parse(path).collect(Collectors.toList());
+    log.info("envelopeList = {}", envelopeList);
+  }
+
   final String feature =
       "Feature: Minimal\n" +
-      "\n" +
-      "  Scenario: minimalistic\n" +
-      "    Given the minimalism\n";
+          "\n" +
+          "  Scenario: minimalistic\n" +
+          "    Given the minimalism\n";
   final Envelope envelope = Envelope.of(new Source("minimal.feature", feature, TEXT_X_CUCUMBER_GHERKIN_PLAIN));
 
   @Test
